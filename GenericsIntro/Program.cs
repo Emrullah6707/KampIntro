d24b4750749064ba97fe90750bbfc88f9148dd5b
EMRULLAH ARSLAN <emrllah6707@gmail.com>
2021-01-20 23:50:34 +0300

    20.01.2021 ders sonu

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList < String > isimler= new MyList<String>();
+            isimler.Add("Emrullah");
+
+        }
+    }
+}
